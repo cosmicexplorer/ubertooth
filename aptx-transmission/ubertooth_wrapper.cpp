1d1f12e1f19604fba57568a3c46ca8d6d01d3443
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-11-11 22:53:05 -0800

    add run-hack task and test it on linux after adding ext libs

--- aptx-transmission/ubertooth_wrapper.cpp
+++ aptx-transmission/ubertooth_wrapper.cpp
@@ -1,3 +1,6 @@
+#include "openaptx.h"
+#include "ubertooth.h"
+
 extern "C" int add_three(int x) {
   return x + 3;
 }
--- pants-plugins/upstreamable/register.py
+++ pants-plugins/upstreamable/register.py
@@ -5,12 +5,13 @@ from __future__ import (absolute_import, division, generators, nested_scopes, pr
 
 import os
 
-from pants.backend.native.config.environment import GCCCToolchain, GCCCppToolchain
+from pants.backend.native.config.environment import GCCCToolchain, GCCCppToolchain, Platform
 from pants.backend.native.targets.native_library import NativeLibrary
 from pants.backend.native.tasks.c_compile import CCompile
 from pants.backend.native.tasks.cpp_compile import CppCompile
 from pants.backend.native.tasks.link_shared_libraries import LinkSharedLibraries
 from pants.backend.native.tasks.native_external_library_fetch import NativeExternalLibraryFetch, NativeExternalLibraryFiles
+from pants.backend.python.tasks.python_run import PythonRun
 from pants.base.build_environment import get_buildroot
 from pants.build_graph.build_file_aliases import BuildFileAliases
 from pants.build_graph.target import Target
@@ -18,6 +19,7 @@ from pants.goal.goal import Goal
 from pants.goal.products import UnionProducts
 from pants.goal.task_registrar import TaskRegistrar as task
 from pants.task.task import Task
+from pants.util.contextutil import environment_as
 from pants.util.memo import memoized_property
 from pants.util.objects import SubclassesOf
 
@@ -81,6 +83,19 @@ class LibusbExternLibHack(Task):
     self.context.products.register_data(NativeExternalLibraryFiles, files_prod)
 
 
+class PythonRunExternLibHack(PythonRun):
+  """A wrapper around PythonRun which sets some hardcoded shared lib paths."""
+
+  def execute(self):
+    loader_env_var_for_plat = Platform.create().resolve_platform_specific({
+      'darwin': lambda: 'DYLD_LIBRARY_PATH',
+      'linux': lambda: 'LD_LIBRARY_PATH',
+    })
+    shared_lib_path = os.path.join(get_buildroot(), 'host/libubertooth/src')
+    with environment_as(**{loader_env_var_for_plat: shared_lib_path}):
+      super(PythonRunExternLibHack, self).execute()
+
+
 def build_file_aliases():
   return BuildFileAliases(
     targets={
@@ -99,3 +114,5 @@ def register_goals():
   task(name='c-hack-for-ctypes', action=CCompileHack).install('native-compile')
   task(name='cpp-hack-for-ctypes', action=CppCompileHack).install('native-compile')
   task(name='shared-libs-libusb', action=LinkSharedLibsHack).install('link')
+
+  task(name='hack-rt-libs-py-run', action=PythonRunExternLibHack).install('run-hack')
