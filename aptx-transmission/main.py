a3d42e339c5035cb0cf7728225459980fc5894ca
Danny McClanahan <1305167+cosmicexplorer@users.noreply.github.com>
2018-11-11 23:30:09 -0800

    add TODO

--- aptx-transmission/main.py
+++ aptx-transmission/main.py
@@ -1,3 +1,5 @@
 from transmit_audio.aptx_hd import ubertooth_wrapper_lib
 
+ubertooth_wrapper_lib.transmit_aptx_hd()
+
 print(ubertooth_wrapper_lib.add_three(2))
--- aptx-transmission/ubertooth_wrapper.cpp
+++ aptx-transmission/ubertooth_wrapper.cpp
@@ -1,6 +1,20 @@
+extern "C" {
 #include "openaptx.h"
 #include "ubertooth.h"
-
-extern "C" int add_three(int x) {
-  return x + 3;
 }
+
+/* TODO:
+   1. (transmitter end) hook up the ubertooth lifecycle methods (as seen in ubertooth-specan.c) to
+      write random data (technically pcm) and use the other laptop to analyze the spectrum to see
+      that stuff is happening!
+   2. make it work on osx!
+   3. (receiver end) try to decode and pull pcm data from the device to pipe somewhere (use sox as
+      in the libopenaptx README!).
+*/
+
+extern "C" void transmit_aptx_hd() {
+  auto ctx = aptx_init(1);
+  aptx_finish(ctx);
+}
+
+extern "C" int add_three(int x) { return x + 3; }
